e638b2a27ab8ad934708302760287c8887662023
Maxime Henaff <maxime2987@gmail.com>
2020-11-16 15:34:09 +0100

    .

--- clear_all.sql
+++ clear_all.sql
@@ -18,7 +18,7 @@ DROP CONTEXT CAL_CTX_ADMIN30;
 DROP PACKAGE set_cal_ctx_pkg_admin30;
 
 DBMS_RLS.DROP_POLICY(
-    object_schema => 'dba',
+    object_schema => 'ADMIN30',
     object_name => 'Contact_ADMIN30',
     policy_name => 'ctc_policy_admin30'
 )
