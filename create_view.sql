05fe4b430d1a608e17b42483c3c1b9c6bb8f167c
MaximeHenaff <36384910+MaximeHenaff@users.noreply.github.com>
2020-11-17 18:24:03 +0100

    ....

--- create_view.sql
+++ create_view.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE VIEW VCal_Evnt_Contact_ADMIN30 AS 
-SELECT * 
+SELECT contact.id as contact_id, Contact.nom, contact.prenom, contact.telephone, contact.courriel, evenement.id as id_evenement, evenement.date_evenement, evenement.lieu, evenement.id_createur, evenement.description  
 FROM Calendrier_ADMIN30 Calendrier
 JOIN Contact_ADMIN30 Contact ON Calendrier.id_contact = Contact.id
 JOIN Evenement_ADMIN30 Evenement ON Calendrier.id_evenement = Evenement.id
